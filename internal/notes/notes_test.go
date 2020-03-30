24e16ec6431efb3b1c34872ed5f233c424dbc5bb
zishone <zishgarces@gmail.com>
2020-03-30 16:59:58 +0800

    Add TestAddNote

--- internal/notes/notes_test.go
+++ internal/notes/notes_test.go
@@ -7,8 +7,22 @@ import (
 )
 
 func TestFetchNotes(t *testing.T) {
-	_, err := notes.FetchNotes()
+	_, fails, err := notes.FetchNotes()
 	if err != nil {
 		t.Errorf("Expected error to be not nil, but got %v", err)
+	}
+	if len(fails) != 0 {
+		t.Errorf("Expected fails length to be 0, but got %v", len(fails))
+	}
+}
+
+func TestAddNote(t *testing.T) {
+	note := notes.Note{}
+	_, fails, err := notes.AddNote(note)
+	if err != nil {
+		t.Errorf("Expected error to be not nil, but got %v", err)
+	}
+	if len(fails) != 0 {
+		t.Errorf("Expected fails length to be 0, but got %v", len(fails))
 	}
 }
